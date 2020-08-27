e9afd381b08286430c8c752fe46343f9223711e6
Gabriele-Araujo <58344362+Gabriele-Araujo@users.noreply.github.com>
2020-08-27 20:28:25 -0300

    Update index.js

---
+++ pages/Agendar/index.js
@@ -0,0 +1,11 @@
+var pessoa = {
+    nome: [''],
+    sobrenome: [''],
+    cpf: ['']
+}
+
+function agendamento(){
+    pessoa.nome = prompt("Nome: ")
+    pessoa.sobrenome = prompt("Sobrenome: ")
+    pessoa.cpf = prompt("CPF: ")
+}
