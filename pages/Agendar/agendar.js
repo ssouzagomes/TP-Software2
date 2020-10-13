061473d6909401791d433cc931ab30180675fcd1
Samuel Souza <48895781+osuricato@users.noreply.github.com>
2020-10-12 21:44:52 -0300

    database

--- pages/Agendar/agendar.js
+++ pages/Agendar/agendar.js
@@ -1,18 +1,10 @@
 var dados = {
-    nome: '',
-    sobrenome: '',
-    cpf: '',
-    data: '',
-    hora: ''
+    nome: [],
+    sobrenome: [],
+    cpf: [],
+    data: [],
+    hora: []
 }
-
-// var nomeArray = []
-// var sobrenomeArray = []
-// var cpfArray = []
-// var dataArray = []
-// var horaArray = []
-
-var indice = -1
 
 function agendar() {
     var nome = document.getElementById('nome').value
@@ -20,29 +12,14 @@ function agendar() {
     var cpf = document.getElementById('cpf').value
     var data = document.getElementById('data').value
     var hora = document.getElementById('hora').value
-    
-    // let values = [nome, sobrenome, cpf, data, hora]
-    // dados.push(values)
 
-    dados.nome = nome
-    dados.sobrenome = sobrenome
-    dados.cpf = cpf
-    dados.data = data
-    dados.hora = hora
-    console.log(dados)
-    // let key = dados.length
+    dados.nome.push(nome)
+    dados.sobrenome.push(sobrenome)
+    dados.cpf.push(cpf)
+    dados.data.push(data)
+    dados.hora.push(hora)
 
-    indice++
-
-    // nomeArray.push(nome)
-    // sobrenomeArray.push(sobrenome)
-    // cpfArray.push(cpf)
-    // dataArray.push(data)
-    // horaArray.push(hora)
-
-    // console.log(nomeArray, sobrenomeArray, cpfArray, dataArray, horaArray)
-
-    saveToStorage(dados, indice)
+    saveToStorage(dados)
 
     document.getElementById('nome').value = ''
     document.getElementById('sobrenome').value = ''
@@ -51,10 +28,14 @@ function agendar() {
     document.getElementById('hora').value = ''
 }
 
-function saveToStorage(dados, indice) {
-    localStorage.setItem(indice, JSON.stringify(dados))
-    // localStorage.setItem(1, JSON.stringify(sobrenomeArray))
-    // localStorage.setItem(2, JSON.stringify(cpfArray))
-    // localStorage.setItem(3, JSON.stringify(dataArray))
-    // localStorage.setItem(4, JSON.stringify(horaArray))
+function saveToStorage(dados) {
+    localStorage.setItem(0, JSON.stringify(dados))
+}
+
+function deletar() {
+    if(window.confirm('Tem certeza que deseja excluir?')) {
+        localStorage.removeItem(0);
+
+        document.getElementsByClassName('mainDados')[0].style.display = 'none'
+    }
 }
--- pages/Login/login.js
+++ pages/Login/login.js
@@ -1,11 +1,7 @@
 function entrar() {
   let usuario = document.getElementById('usuario').value
   let senha = document.getElementById('senha').value
-  console.log(usuario, senha)
-  db.transaction(function(inserir){
-    inserir.executeSql("INSERT INTO users (usuario, senha) VALUES (?,?)", [usuario, senha])
-});
-
+  
   if(usuario == 'tnis' && senha == 1234) {
     document.getElementById('form').action = 'pages/Principal/principal.html'
   } else {
