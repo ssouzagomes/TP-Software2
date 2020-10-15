fa80b20018ea13e272d9704cebd6e1ccfc0dc5c4
Gabriele-Araujo <58344362+Gabriele-Araujo@users.noreply.github.com>
2020-10-15 11:24:45 -0300

    Update login.js

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -2,6 +2,7 @@ function entrar() {
   let usuario = document.getElementById('usuario').value
   let senha = document.getElementById('senha').value
   
+  //Validação
   if(usuario == 'tnis' && senha == 1234) {
     document.getElementById('form').action = 'pages/Principal/principal.html'
   } else {
